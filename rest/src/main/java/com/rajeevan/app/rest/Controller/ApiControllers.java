be54f9ac7d9842ce8792e9c5398cc39ee41db74c
Raj162002 <Ramkumarrajeevan16@gmail.com>
2024-04-22 21:26:41 +0530

    Added saveUser before connecting to postman

--- rest/src/main/java/com/rajeevan/app/rest/Controller/ApiControllers.java
+++ rest/src/main/java/com/rajeevan/app/rest/Controller/ApiControllers.java
@@ -4,6 +4,7 @@ import com.rajeevan.app.rest.Models.User;
 import com.rajeevan.app.rest.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +21,10 @@ public class ApiControllers {
     @GetMapping(value="/users")
     public List<User> getUsers(){
         return userRepo.findAll();
+    }
+    @PostMapping(value="/save")
+    public String saveUser(User user){
+        userRepo.save(user);
+        return "Saved ....";
     }
 }
